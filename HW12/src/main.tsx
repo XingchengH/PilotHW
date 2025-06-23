59b16d0f1f45a8d668b92cea88e56ebf8f7a8346
Xingcheng H <Huangxcxc@gmail.com>
2025-06-22 20:06:08 -0400

    Adding fonts

--- HW12/src/main.tsx
+++ HW12/src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store.tsx";
 
-import App from "./App.tsx";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/assets/fonts/fonts.css";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
